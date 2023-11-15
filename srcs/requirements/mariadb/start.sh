2a8665b8e661e4bccc41d873cda885f8bf02b643
Felicien410 <97636902+Felicien410@users.noreply.github.com>
2023-11-15 08:47:42 +0100

    pour eviter de la d

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,34 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    start.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: feliciencatteau <feliciencatteau@studen    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/06/06 16:41:34 by shovsepy          #+#    #+#              #
+#    Updated: 2023/11/13 20:39:14 by feliciencat      ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/sh
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+chown -R mysql:mysql /var/lib/mysql
 
+# Vérifier si la base de données a déjà été initialisée
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	
-	chown -R mysql:mysql /var/lib/mysql
-
-	# Initializing the database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-
-	file=`mktemp`
-	if [ ! -f "$file" ]; then
-		return 1
-	fi
-
-	cat << EOF > $file
-
-# Création de la base de données et de l'utilisateur
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
-EOF
-	# Creating the environment
-	/usr/bin/mysqld --user=mysql --bootstrap < $file
-	rm -f $file
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql 
 fi
 
-exec /usr/bin/mysqld --user=mysql --console
+mysqld_safe --datadir='/var/lib/mysql' &
+
+# Augmenter le délai d'attente
+sleep 20
+
+
+mysql -u root -e "DELETE FROM	mysql.user WHERE User='';"
+mysql -u root -e "DROP DATABASE IF EXISTS test;"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+
+# Allowing network connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -1,2 +1,31 @@
-clear_env = no
-listen=wordpress:9000
+#!/bin/sh
+
+# Le chemin où se trouve votre wp-config.php (dans le conteneur)
+WP_CONFIG="/var/www/html/wp-config.php"
+
+# Télécharger la clé unique de salage de WordPress
+WP_SALT=$(wget -qO- https://api.wordpress.org/secret-key/1.1/salt/)
+
+# Utiliser les variables d'environnement pour les informations de la base de données
+DB_NAME=${DB_NAME:-wordpress}
+DB_USER=${DB_USER:-user}
+DB_PASSWORD=${DB_PASSWORD:-password}
+DB_HOST=${DB_HOST:-mariadb}
+
+# Créer le fichier wp-config.php à partir de wp-config-sample.php
+cp /var/www/html/wp-config-sample.php "$WP_CONFIG"
+
+# Configurer les informations de base de données
+sed -i "s/database_name_here/$DB_NAME/" "$WP_CONFIG"
+sed -i "s/username_here/$DB_USER/" "$WP_CONFIG"
+sed -i "s/password_here/$DB_PASSWORD/" "$WP_CONFIG"
+sed -i "s/localhost/$DB_HOST/" "$WP_CONFIG"
+
+# Configurer les clés de salage uniques
+printf '%s\n' "g/$table_prefix = 'wp_';/r /dev/stdin" . x | ex "$WP_CONFIG" <<< "$WP_SALT"
+
+# Ajouter des configurations supplémentaires si nécessaire
+# ...
+
+# Démarrer PHP-FPM
+php-fpm8 -F
