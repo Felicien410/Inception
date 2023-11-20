52764ab3b5f77979219bcc023759d4b9c84e296f
Felicien410 <97636902+Felicien410@users.noreply.github.com>
2023-11-20 10:24:06 +0100

    dd

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,47 +1,16 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    start.sh                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: feliciencatteau <feliciencatteau@studen    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/06/06 16:41:34 by shovsepy          #+#    #+#              #
-#    Updated: 2023/11/17 13:41:33 by feliciencat      ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/sh
 
-chown -R mysql:mysql /var/lib/mysql
+mkdir -p /Users/feliciencatteau/data/wordpress
+mkdir -p /Users/feliciencatteau/data/mariadb
 
-# Vérifier si la base de données a déjà été initialisée
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --ldata=/var/lib/mysql 
-fi
+/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
 
-mysqld_safe --datadir='/var/lib/mysql' &
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
 
-# Augmenter le délai d'attente
-sleep 20
-
-echo "DB_NAME: ${DB_NAME}"
-echo "DB_USER: ${DB_USER}"
-echo "DB_PASSWORD: ${DB_PASSWORD}"
-
-mysql -u root -e "DELETE FROM	mysql.user WHERE User='';"
-mysql -u root -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-mysqladmin shutdown
-
-
-# Allowing network connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-exec /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+pkill mysqld
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
